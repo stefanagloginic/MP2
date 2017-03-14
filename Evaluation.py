7640a07ebf7a355fb61ddf2fb132f2f983f297d5
stefanagloginic <stefana.gloginic@gmail.com>
2017-03-14 11:25:51 -0700

    Add files via upload

--- Evaluation.py
+++ Evaluation.py
@@ -57,43 +57,6 @@ def get_my_player(board):
 """def evaluation(board):
 	return random.randint(0, 1000)
 """
-'''def evaluation(board):
-	new_matrix = np.asmatrix(board)
-
-	diagonals = [new_matrix[::-1,:].diagonal(i) for i in range(-new_matrix.shape[0]+1, new_matrix.shape[1])]
-	diagonals.extend(new_matrix.diagonal(i) for i in range(new_matrix.shape[1]-1, -new_matrix.shape[0],-1))
-
-	diag = []
-
-	for d in diagonals:
-		diag = d.tolist()[0]
-
-	rows = []
-	cols = []
-	diag_row_col_arr = []
-
-	for x in range(board.size):
-		rows.append(np.asarray(new_matrix[i,:]).reshape(-1).tolist()[0])
-		cols.append(np.asarray(new_matrix[:,i]).reshape(-1).tolist())
-
-	diag_row_col_arr=diag+cols+rows
-
-	final_array = []
-
-	for arr in diag_row_col_arr:
-		#want to extract those arrays that arent of length at LEAST 5 because those are the potential winning moves
-		if(len(arr) >= 5):
-			final_array += arr
-
-	white_connect = 0
-	black_connect = 0
-
-	#now create sliding door effect to check for potential connections and ignore blocked connection
-	for arr in final_array:
-		for val in range(len(arr)):
-			for slide in range(5)
-			
-'''
 
 def evaluation(board):
 	directions = [(1,0), (0,1), (1,1), (1,-1)]
@@ -101,17 +64,13 @@ def evaluation(board):
 	total_sum_white = 0
 	total_sum_black = 0
 
-	#greatest_stored_black_connect = 0
-	#greatest_stored_white_connect = 0
-
 	for coordinates in board.black: #this for loop takes care of moving in both directions
 		black_connect = 0
 		for pair in directions:
 			black_connect += 1 + board.check_connection(coordinates, pair, b.Board.black_player)
 			(row, col) = pair
 			black_connect += board.check_connection(coordinates, (-row,-col), b.Board.black_player)
-		#if(black_connect > greatest_stored_black_connect):
-		#	greatest_stored_black_connect = black_connect
+
 		total_sum_black += pow(4, black_connect) #connect will always be at least 1. 
 		if(black_connect == 4):
 			pow(4, black_connect)
@@ -124,25 +83,15 @@ def evaluation(board):
 			white_connect += 1 + board.check_connection(coordinates, pair, b.Board.white_player)
 			(row, col) = pair
 			white_connect += board.check_connection(coordinates, (-row,-col), b.Board.white_player)
-		#if(white_connect > greatest_stored_white_connect):
-		#	greatest_stored_white_connect = white_connect
 
 		total_sum_white += pow(4, white_connect) 
 		if(white_connect == 5):
 			total_sum_white += pow(4, white_connect)
 
-#	if((greatest_stored_black_connect >= 3 and board.is_my_opponent(b.Board.black_player))): #defend vs opponents threat
-#		total_sum_black *= 10
-
-#	if(greatest_stored_white_connect >= 3 and board.is_my_opponent(b.Board.white_player)): #defend vs opponents threat
-#		total_sum_white *= 10
-
 	if(board.player == board.white_player):
-		#print("subtraction: " + str(total_sum_white-total_sum_black))
 		return(total_sum_white-total_sum_black)
 
 	#I will take the max of the return value, thus prioritizing if my opponent has a better connection
-	#print("subtraction: " + str(total_sum_black-total_sum_white))
 	return(total_sum_black-total_sum_white)
 
 
